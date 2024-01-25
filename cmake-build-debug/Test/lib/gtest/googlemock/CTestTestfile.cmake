663691d10a54606fe8b8bb95d0253f8122c4bdf2
DonatiFed <federico.donati@stud.unifi.it>
2024-01-25 23:28:13 +0100

    not working build

---
+++ cmake-build-debug/Test/lib/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Donat/CLionProjects/laboratorio programmazione/Test/lib/gtest/googlemock
+# Build directory: C:/Users/Donat/CLionProjects/laboratorio programmazione/cmake-build-debug/Test/lib/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ cmake-build-debug/Test/lib/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Donat/CLionProjects/laboratorio programmazione/Test/lib/gtest/googletest
+# Build directory: C:/Users/Donat/CLionProjects/laboratorio programmazione/cmake-build-debug/Test/lib/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
